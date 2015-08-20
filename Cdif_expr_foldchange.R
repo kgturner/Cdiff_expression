c0652643f8954a6328c3ecfa70a3bee84c5fad2c
Kathryn G Turner <kathryn.g.turner@gmail.com>
2015-08-19 21:24:44 -0700

    picking out example genes

--- Cdif_expr_foldchange.R
+++ Cdif_expr_foldchange.R
@@ -2,7 +2,7 @@
 #7/20/15
 
 #load data
-lrtair <- read.table("LRTsandTAIRaccessions.txt", header=T, sep="\t") lrt pvals and tair accessions
+lrtair <- read.table("LRTsandTAIRaccessions.txt", header=T, sep="\t") #lrt pvals and tair accessions
 
 #up and down data
 
@@ -13,12 +13,43 @@ sigOT0summaryNE <- read.table(file="sigOrigin_popMeans_sumT0NE.txt", header=T)
 sigOT0tair <- merge(sigOT0summaryNE, lrtair, all.x=TRUE)
 sigOT0tair$folddiff <- sigOT0tair$InvExprVal/sigOT0tair$NatExprVal #fold differences
 
+#log-fold difference
 extremefold <- subset(sigOT0tair, (folddiff>=1.5|folddiff<=0.67)&TAIRaccessions!="NA,NA") 
 #extreme fold difference >1.5 (1.5 fold increase in inv rel to nat) or <.67 (1.5 fold decrease in inv rel to nat);  
 #only ones with TAIR accessions:
 # AT3G16630 >1.5 fold higher in inv
 # AT5G67280 >1.5 fold higher in inv
 # AT2G46100 >1.5 fold lower in inv
+
+#sig genes in particular pathways
+#etheylene, down in inv
+# GO:0007165  signal transduction
+GO:0009873  ethylene-activated signaling pathway
+GO:0071369	cellular response to ethylene stimulus
+
+eth <- subset(sigOT0tair, InvDefDn=T&TAIRaccessions!="NA,NA") 
+eth2 <- grep("0009873|0071369", eth$GOterms, perl=TRUE, value=FALSE)
+ethsub <- subset(eth, row.names(eth)%in%eth2)
+
+
+str(eth$GOterms)
+
+# ethsub <- for (i in 1:nrow(eth)) {
+#   if (grepl("0007165|0009873|0071369",eth[i,"GOterms"])){
+#   return(eth[i,])}
+#   
+# }
+
+
+
+
+
+
+
+grep("a+", c("abc", "def", "cba a", "aa"), perl=TRUE, value=TRUE)
+
+
+
 
 
 ####induced difference, timepoint 2, drought####
