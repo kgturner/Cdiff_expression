bb188ea8da33c0958524fdf8957d23934de3ef51
Kathryn G Turner <kathryn.g.turner@gmail.com>
2016-01-15 13:23:49 -0700

    made table of overlaping genes between my origin*trt or trt genes and Marchand et al list of genes in drought regulatory network of sunflower

--- Cdif_expr_comparisons.R
+++ Cdif_expr_comparisons.R
@@ -29,83 +29,46 @@ CCopcRes$GO.ID %in% invsyn$GO.ID
 tair <- read.table("LRTsandTAIRaccessions.txt", header=T, sep="\t")
 Marchand <- read.table ("nph12818TableS3.txt", header=T, sep="\t")
 
+##make tair list from my data
 #split TAIRaccessions column
 tair$TAIRaccessions <- as.character(tair$TAIRaccessions)
-# unlist(strsplit("a.b.c", ".", fixed = TRUE))
-
 splitdat = do.call("rbind", strsplit(tair$TAIRaccessions, ","))
 splitdat = data.frame(apply(splitdat, 2, as.character))
-# names(splitdat) = paste("trial", 1:4, sep = "")
 head(splitdat)
-
 tair <- cbind(tair, splitdat)
+
 #subset with significant range*treatment interaction or treatment
 tair_dr <- subset(tair, intQ<=0.05 | trtQ<=0.05)
+tair_dr <- droplevels(tair_dr)
+tair_dr$X1 <- as.character(tair_dr$X1)
+tair_dr$X2 <- as.character(tair_dr$X2)
+tairList <- unique(c(tair_dr$X1, tair_dr$X2))
+
+#check for duplicates
+n_occur <- data.frame(table(tairList))
+n_occur[n_occur$Freq > 1,]
+tairList %in% n_occur$Var1[n_occur$Freq > 1]
+
+##make Marchand list
+Marchand$AGI_ID.from.HU <- as.character(Marchand$AGI_ID.from.HU)
+Marchand$AGI_ID.from.HaT13l <- as.character(Marchand$AGI_ID.from.HaT13l)
+MarchandList <- unique(c(Marchand$AGI_ID.from.HU, Marchand$AGI_ID.from.HaT13l))
+
+#check for duplicates
+n_occur <- data.frame(table(MarchandList))
+n_occur[n_occur$Freq > 1,]
+MarchandList %in% n_occur$Var1[n_occur$Freq > 1]
 
 #compare lists
-#AT codes from Marchand in $AGI_ID.from.HU and $AGI_ID.from.HaT13l
-#and in tair from $X1 and $X2
+inGRNlist <- intersect(tairList, MarchandList) #52 unique TAIR accessions
 
-inGRN <- subset(tair_dr, X1 %in% Marchand$AGI_ID.from.HU)
-inGRN <- rbind(inGRN, subset(tair, X2 %in% Marchand$AGI_ID.from.HU))
-inGRN <- rbind(inGRN, subset(tair, X1 %in% Marchand$AGI_ID.from.HaT13l))
-inGRN <- rbind(inGRN, subset(tair, X2 %in% Marchand$AGI_ID.from.HaT13l))
-inGRN <- droplevels(inGRN)
-
-inGRNlist <- unique(c(levels(inGRN$X1),levels(inGRN$X2)))
-# inGRNlist <- unique(inGRNlist)
-inGRNlist <- inGRNlist[1:89] #remove NA
-
-MarchandOverlap <- subset(Marchand, AGI_ID.from.HU %in% inGRNlist | AGI_ID.from.HaT13l %in% inGRNlist)
-MarchandOverlap <- MarchandOverlap[,1:9]
-tairOverlap <- subset(tair_dr, X1 %in% inGRNlist | X2 %in% inGRNlist)
-tairOverlap <- droplevels(tairOverlap)
-
+##make table
 #just accessions and descriptions that show up in both lists:
-#not working yet...
-MarchandComp <- as.data.frame(inGRNlist)
-final <- merge(MarchandComp, Marchand, by.x="inGRNlist", by.y="AGI_ID.from.HU", all.x=T)
-final <- merge(final, Marchand, by.x="inGRNlist", by.y="AGI_ID.from.HaT13l", all.x=T)
-# final$name.from.Hu %in% final$name.from.HaT13l
+final <- subset(Marchand, AGI_ID.from.HU %in% inGRNlist |AGI_ID.from.HaT13l %in% inGRNlist)
+final <- final[,4:9]
+write.table(final, file="MarchandGRNoverlap.txt", sep="\t")
 
+#checking duplication between columns - some easy editing in xcel
+final.1 <- subset(final, AGI_ID.from.HU == AGI_ID.from.HaT13l) #some NAs in once col but not other
+final.1 <- subset(final, name.from.Hu == name.from.HaT13l) #some NAs in once col but not other
 
-#attepmts to make fancier table
-colnames(tairOverlap)[8] <- "AT1"
-colnames(tairOverlap)[9] <- "AT2"
-tairMarch.1 <- tairOverlap[,c(1,8)]
-tairMarch.2<- tairOverlap[,c(1,9)]
-tairMarch.2 <- subset(tairMarch.2, AT2!="NA")
-colnames(tairMarch.1)[2] <- "TAIRaccession"
-colnames(tairMarch.2)[2] <- "TAIRaccession"
-tairMarch <- unique(rbind(tairMarch.1, tairMarch.2))
-rownames(tairMarch) <- tairMarch$TAIRaccession
-
-
-March <- MarchandOverlap[,c(4:8)]
-
-
-tairMarch <- merge(tairMarch, March, by.x="AT1", by.y="AGI_ID.from.HU", all.x=T)
-tairMarch2 <- merge(tairMarch, March, by.x="AT2", by.y="AGI_ID.from.HU", all.x=T)
-# tairMarch3<- merge(tairMarch, March, by.x="AT1", by.y="AGI_ID.from.HaT13l", all.x=T)
-# tairMarch4<- merge(tairMarch, March, by.x="AT2", by.y="AGI_ID.from.HaT13l", all.x=T)
-
-tairMarchAll <- merge(tairMarch, tairMarch2,by="Contig")# 
-# # tairMarchall <- merge(tairMarch, tairMarch2, by="Contig", all=T)
-# tairMarchall <- 
-
-squish <- function(dat=dat, cols=cols, newcol="newcol"){
-  
-  dat$temp <- NA
-  
-  for (i in dat[,cols]){
-    ## where is temp NA?
-    ss <- which(is.na(dat$temp))
-    ## what are the values of i there? put them in temp
-    dat$temp[ss] <- i[ss] 
-  }
-  
-  names(dat)[names(dat)=="temp"] <- newcol
-  
-  return(subset(dat,select=ncol(dat)))
-}
-test <- squish(dat=tairMarch2, cols=c(4,8), newcol="alt")
